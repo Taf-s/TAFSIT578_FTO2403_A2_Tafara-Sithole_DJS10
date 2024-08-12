52e72cb5210faf224d7736a4b06a0deb722a7844
taf-s <tafara.sithole@gmail.com>
2024-08-12 14:51:59 +0200

    implemented fetch requests and state management

---
+++ blog-post-app/src/FetchPosts.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const useFetchPosts = () => {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch posts");
+        }
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        setError("Data fetching failed");
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return { posts, error };
+};
+
+export default useFetchPosts;
